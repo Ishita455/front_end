89949014c52dc2c7385db030c37f77ec1db0a3d1
Ishita455 <ishyrat123@gmail.com>
2021-11-18 23:25:02 +0530

    JS basics

---
+++ JS-MASTERY/1.3-operators/operators.js
@@ -0,0 +1,30 @@
+// alert('hello');
+
+// Operator and Operand
+var num1 = 22;
+console.log(num1);
+num1 = -num1;   // -  is the unary operator and num1 is operand, bcoz the - is working on 1 variable is called uninary opreator
+console.log(num1);
+
+var num2 = 35;
+var result = 2+num2;    // + is binary operator and num2 is operand as its working on 2 variables
+console.log(result);
+
+var x = 5;
+var y = 2;
+//Arithematic opeations
+console.log("Result of addn is"+(x+y));
+console.log("Result of sub is"+(x-y));
+console.log("Result of multi is"+(x*y));
+console.log("Result of div is"+(x/y));
+console.log("Result of exponent is"+(x**y));
+console.log("Result of x++ is"+(x++));
+
+console.log("hello"+"-"+"world")
+
+var firstName = "John";
+var lastName = "Doe";
+
+console.log("your 1st name is "+firstName+"and last name is "+lastName)
+console.log(`your 1st name is ${firstName} and last name is ${lastName}`)
+
